291afe4cc7c804abb7bffe8195518b1d9caf7167
Great Ugochukwu Amaizu <greatamaizu@gmail.com>
2025-04-06 01:29:20 +0100

    #fix: wrong import of path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import NavBar from './components/navbar'
+import NavBar from './components/NavBar'
 import { Outlet } from 'react-router-dom'
 import SearchBar from './components/searchBar'
 
